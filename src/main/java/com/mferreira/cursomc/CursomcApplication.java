b9d40e05cb4975a21689a604c8e1461038c59178
Miguel Ferreira <miguelfferreira93@gmail.com>
2018-03-04 13:41:42 +0000

    App Instantiation

--- src/main/java/com/mferreira/cursomc/CursomcApplication.java
+++ src/main/java/com/mferreira/cursomc/CursomcApplication.java
@@ -1,12 +1,33 @@
 package com.mferreira.cursomc;
 
+import org.assertj.core.util.Arrays;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.mferreira.cursomc.domain.Categoria;
+import com.mferreira.cursomc.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+	
+		categoriaRepository.save(cat1);
+		categoriaRepository.save(cat2);
+		
+	}
 }
+
