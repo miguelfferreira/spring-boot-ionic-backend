e12aaa5df8f1a78e8d8c2e53bb797a1732f7a1b3
Miguel Ferreira <miguelfferreira93@gmail.com>
2018-03-06 23:11:29 +0000

    ItemPedido e ItemPedidoPK

---
+++ src/main/java/com/mferreira/cursomc/repositories/ItemPedidoRepository.java
@@ -0,0 +1,11 @@
+package com.mferreira.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.mferreira.cursomc.domain.ItemPedido;
+
+@Repository
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer>{
+
+}
