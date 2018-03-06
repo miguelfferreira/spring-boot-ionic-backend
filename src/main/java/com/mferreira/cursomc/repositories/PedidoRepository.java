6dafce662923f2d5e41786fc8bf69dca390d931d
Miguel Ferreira <miguelfferreira93@gmail.com>
2018-03-06 21:05:36 +0000

    Pedido, EstadoPagamento e Pagamento

---
+++ src/main/java/com/mferreira/cursomc/repositories/PedidoRepository.java
@@ -0,0 +1,11 @@
+package com.mferreira.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.mferreira.cursomc.domain.Pedido;
+
+@Repository
+public interface PedidoRepository extends JpaRepository<Pedido, Integer>{
+
+}
