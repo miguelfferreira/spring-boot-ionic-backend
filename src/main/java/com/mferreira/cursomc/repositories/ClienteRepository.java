60f5cd7487e6ad462c2cbc485edae5dee2e096d1
Miguel Ferreira <miguelfferreira93@gmail.com>
2018-03-10 19:26:01 +0000

    Custom Validation: Email

--- src/main/java/com/mferreira/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/mferreira/cursomc/repositories/ClienteRepository.java
@@ -3,6 +3,7 @@ package com.mferreira.cursomc.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.mferreira.cursomc.domain.Cliente;
 
@@ -11,4 +12,7 @@ public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
 
 	@Query("SELECT c FROM Cliente c where c.id = ?1")
 	Cliente findByClienteId(Integer id);
+	
+	@Transactional(readOnly=true)
+	Cliente findByEmail(String email);
 }
