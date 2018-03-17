d4fd3c5cc2ae9cf3dc4d0e058fc3ac0dd6465ebd
Miguel Ferreira <miguelfferreira93@gmail.com>
2018-03-17 23:22:34 +0000

    Url

--- src/main/java/com/mferreira/cursomc/domain/Cliente.java
+++ src/main/java/com/mferreira/cursomc/domain/Cliente.java
@@ -54,6 +54,8 @@ public class Cliente implements Serializable{
 	@OneToMany(mappedBy="cliente")
 	private List<Pedido> pedidos = new ArrayList<>(); 
 	
+	private String imageUrl;
+	
 	public Cliente() {
 		addPerfil(Perfil.CLIENTE);
 	}
@@ -173,5 +175,12 @@ public class Cliente implements Serializable{
 			return false;
 		return true;
 	}
-	
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
 }
