b7f501eb3398fdb02d908028d468d23f02fd185d
Miguel Ferreira <miguelfferreira93@gmail.com>
2018-03-18 00:05:49 +0000

    Images

--- src/main/java/com/mferreira/cursomc/services/ClienteService.java
+++ src/main/java/com/mferreira/cursomc/services/ClienteService.java
@@ -1,9 +1,11 @@
 package com.mferreira.cursomc.services;
 
+import java.awt.image.BufferedImage;
 import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -45,6 +47,12 @@ public class ClienteService {
 	
 	@Autowired
 	private S3Service s3Service;
+	
+	@Autowired
+	private ImageService imageService;
+	
+	@Value(value="${img.prefix.client.profile}")
+	private String prefix;
 		
 	public Cliente find(Integer id) {
 		
@@ -121,15 +129,8 @@ public class ClienteService {
 		if(user == null) {
 			throw new AuthorizationException("Acesso negado");
 		}
-		
-		URI uri = s3Service.uploadFile(multipartFile);
-		
-		Cliente cli = repo.findByClienteId(user.getId());
-		cli.setImageUrl(uri.toString());
-		repo.save(cli);
-		
-		return uri;
+		BufferedImage jpgImage = imageService.getJpgImageFromFile(multipartFile);
+		String fileName = prefix + user.getId() + ".jpg";
+		return s3Service.uploadFile(imageService.getInputStream(jpgImage, "jpg"), fileName, "image");
 	}
-	
-	
 }
